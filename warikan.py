24a8968be8ba8e69814c1c6fecce8c44580c58b7
rimurun <rimuru930@gmail.com>
2020-08-19 15:53:56 +0900

    リファクタリング:使いやすいように変更を加えた

--- warikan.py
+++ warikan.py
@@ -1,13 +1,8 @@
-# table1: 1500, 3人
-# table2: 2000, 3人
-# table3: 3647, 4人
-
-
 def warikan(amount, number_of_people):
     amount = amount // number_of_people
     return f'一人あたり:{amount}円, 端数:{amount % number_of_people}円'
 
 
-print(warikan(1500, 3))
-print(warikan(2000, 3))
-print(warikan(3647, 4))
+print(warikan(amount=1500, number_of_people=3))
+print(warikan(amount=2000, number_of_people=3))
+print(warikan(amount=3647, number_of_people=3))
